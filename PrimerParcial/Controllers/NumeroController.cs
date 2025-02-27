1eb74a551e69efd811729bb8e4db2762c239a7a4
cfern_jbm9xrr <cfern_jbm9xrr@FER>
2025-02-26 19:55:06 -0600

    Agregar archivos de proyecto.

---
+++ PrimerParcial/Controllers/NumeroController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimerParcial.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace PrimerParcial.Controllers
+{
+    public class NumeroController : Controller
+    {
+        public IActionResult Calcular()
+        {
+            return View("NumeroView");
+        }
+
+        public IActionResult NumRand(NumeroModel ObjNumero)
+        {
+            if (ObjNumero.Num1 > ObjNumero.Num2)
+            {
+                ObjNumero.Resultado = "El Numero Minimo no puede ser mayor al Numero Maximo";
+                return View("NumeroView", ObjNumero);
+            }
+            else
+            {
+                Random NumRand = new Random();
+                ObjNumero.Num3 = NumRand.Next(ObjNumero.Num1, ObjNumero.Num2 + 1);
+                ObjNumero.Resultado = "El numero al azar es: " + ObjNumero.Num3;
+                return View("NumeroView", ObjNumero);
+
+            }
+        }
+    }
+}
---
+++ PrimerParcial/Models/NumeroModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrimerParcial.Models
+{
+    public class NumeroModel
+    {
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+        public int Num3 { get; set; }
+        public string Resultado { get; set; }
+    }
+}
